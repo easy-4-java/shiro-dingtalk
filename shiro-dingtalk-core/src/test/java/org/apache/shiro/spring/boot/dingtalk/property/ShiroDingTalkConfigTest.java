5fb79d875c6bd1296a74637abc766bf51b6b180d
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-21 11:42:03 +0800

    style(pom): 格式化多个模块的pom.xml文件
    - 在多个模块的pom.xml文件中添加适当的XML换行和缩进
    - 修复 dreamina-java-sdk 中 requireMavenVersion 和 requireJavaVersion 的消息格式
    - 统一所有pom.xml文件中的XML结构和可读性
    - 重命名 ShiroDingTalkPropertiesTest 类为 ShiroDingTalkConfigTest

--- shiro-dingtalk-core/src/test/java/org/apache/shiro/spring/boot/dingtalk/property/ShiroDingTalkConfigTest.java
+++ shiro-dingtalk-core/src/test/java/org/apache/shiro/spring/boot/dingtalk/property/ShiroDingTalkConfigTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
  *
  * @author [@Loong Wan](https://github.com/loong10k)
  */
-class ShiroDingTalkPropertiesTest {
+class ShiroDingTalkConfigTest {
 
     @Test
     @DisplayName("ShiroDingTalkCropAppProperties setters and getters")
